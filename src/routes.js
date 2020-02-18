412fb32e57ef6dd8a4142836c4f0ee8ab7af2fad
FlavioBS <bzsflavio@gmail.com>
2020-02-17 23:10:39 -0300

    struct project start

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
+routes.get('/', (req, res) => res.json({ message: 'Flávio Lindoooo' }));
 
 export default routes;
