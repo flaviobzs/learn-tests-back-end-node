c4711142ea82df2cd2f7b39c0d144b44622d3157
FlavioBS <bzsflavio@gmail.com>
2020-02-19 01:36:49 -0300

    encrypt for password users

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,35 +1,46 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
-import truncate from '../util/truncate'
+import User from '../../src/app/models/User';
+import truncate from '../util/truncate';
 
 describe('User', () => {
-
   beforeEach(async () => {
     await truncate();
   });
 
-  it('should be able to register', async () => {
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Flávio',
+      email: 'bzsflavio@gmail.com',
+      password: '123456',
+    });
 
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
+  it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
         name: 'Flávio',
         email: 'bzsflavio@gmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
-
     await request(app)
       .post('/users')
       .send({
         name: 'Flávio',
         email: 'bzsflavio@gmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     const response = await request(app)
@@ -37,10 +48,9 @@ describe('User', () => {
       .send({
         name: 'Flávio',
         email: 'bzsflavio@gmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     expect(response.status).toBe(400);
-
   });
 });
